8b13e8ddad9ea707ed4cdcdfb7fd33a36463b4da
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-01-30 22:27:31 +0930

    add copyright

--- ngs_pipeline/cmds/showcmds.py
+++ ngs_pipeline/cmds/showcmds.py
@@ -1,6 +1,14 @@
+# showcmds.py - ngs-pipeline command line
+# [https://github.com/vivaxgen/ngs-pipeline]
+
+__copyright__ = "(c) 2023, Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+
+# to improve the responsiveness during bash autocomplete, do not import heavy
+# modules (such as numpy, pandas, etc) here, but instead import them within the
+# functions that require the respective heavy modules
 
 from ngs_pipeline import cout, cerr, arg_parser
-from ngs_pipeline.cmds import list_commands
 
 
 def init_argparser():
@@ -10,6 +18,8 @@ def init_argparser():
 
 
 def main(args):
+
+    from ngs_pipeline.cmds import list_commands
 
     cout("Available commands:")
     for cmd in list_commands():
