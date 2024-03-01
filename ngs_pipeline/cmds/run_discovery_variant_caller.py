d42f29f0dd4afa6b378198e4e2adb10c96758815
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-03-01 15:58:51 +0930

    add full automatic discovery variant calling flow

---
+++ ngs_pipeline/cmds/run_discovery_variant_caller.py
@@ -0,0 +1,61 @@
+# run_discovery_variant_caller.py - ngs-pipeline command
+# [https://github.com/vivaxgen/ngs-pipeline]
+
+__copyright__ = "(C) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+
+# to improve the responsiveness during bash autocomplete, do not import heavy
+# modules (such as numpy, pandas, etc) here, but instead import them within the
+# functions that require the respective heavy modules
+
+import os
+import pathlib
+from ngs_pipeline import (cerr, cexit, arg_parser, get_snakefile_path,
+                          snakeutils)
+
+# this is a wrapper to run sample_preparation, individual sample genotyping,
+# and joint variant calling in a single command
+
+
+def init_argparser():
+    p = snakeutils.init_argparser('run whole steps of discovery variant caller')
+    p.add_argument('-u', '--underscore', type=int, default=0,
+                   help='no of consecutive underscore to be stripped from '
+                   'filenames to form sample code, counted in reverse')
+    
+    p.add_argument('-o', '--outdir',
+                   help='output directory')
+    p.add_argument('infiles', nargs='+',
+                   help="read files")
+    
+    return p
+
+
+def run_discovery_variant_valler(args):
+
+    import ngs_pipeline
+    from ngs_pipeline import get_snakefile_path, snakeutils
+    
+    config=dict(
+        underscore=args.underscore,
+        outdir=args.outdir,
+        infiles=args.infiles,
+    )
+
+    args.snakefile = get_snakefile_path(
+        'discovery_variant_calling.smk',
+        from_module=ngs_pipeline
+    )
+
+    status, elapsed_time = snakeutils.run_snakefile(args, config=config)
+
+    if not status:
+        cerr('[WARNING: full run of discovery variant calling '
+             'did not successfully complete]')
+    cerr(f'[Finish full run of discovery variant calling (time: {elapsed_time})]')
+
+
+def main(args):
+    run_discovery_variant_valler(args)
+
+# EOF
---
+++ ngs_pipeline/rules/discovery_variant_calling.smk
@@ -0,0 +1,67 @@
+# discovery_variant_caller.smk - ngs-pipeline rules
+# [https://github.com/vivaxgen/ngs-pipeline]
+
+__copyright__ = "(C) 2024 Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+
+outdir = config['outdir']
+infiles = config['infiles']
+
+
+rule all:
+    input:
+        f'{outdir}/metafile/manifest.tsv',
+        f'{outdir}/analysis/._completed_',
+        f'{outdir}/joint/._completed_'
+
+
+rule generate_manifest:
+    localrule: True
+    output:
+        f'{outdir}/metafile/manifest.tsv'
+    params:
+        underscore = config['underscore'],
+        infiles = ' '.join(infiles)
+    shell:
+        'ngs-pl generate-manifest -o {output} -u {params.underscore} --pause 3 '
+        '{infiles} '
+
+
+rule run_prepare_sample_directory:
+    localrule: True
+    input:
+        f'{outdir}/metafile/manifest.tsv'
+    output:
+        f'{outdir}/analysis/._prepared_'
+    params:
+        extra_opts = '--force'
+    shell:
+        'ngs-pl prepare-sample-directory {params.extra_opts} '
+        '-o {outdir}/analysis -i {input} .'
+        '&& touch {output}'
+
+
+rule run_sample_variant_caller:
+    localrule: True
+    input:
+        f'{outdir}/analysis/._prepared_'
+    output:
+        f'{outdir}/analysis/._completed_'
+    shell:
+        'ngs-pl run-sample-variant-caller --force --no-config-cascade '
+        '--target all {outdir}/analysis '
+        '&& touch {output}'
+
+
+rule run_joint_variant_caller:
+    localrule: True
+    input:
+        f'{outdir}/analysis/._completed_'
+    output:
+        f'{outdir}/joint/._completed_'
+    shell:
+        'ngs-pl run-joint-variant-caller --force --no-config-cascade '
+        '-o {outdir}/joint {outdir}/analysis/ '
+        '&& touch {output}'
+
+# EOF
