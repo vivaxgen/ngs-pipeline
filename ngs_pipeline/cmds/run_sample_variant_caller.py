f88c7ebf347bb8aa115041cb414e4b0cc0180549
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-07-06 14:57:33 +0930

    feat: run samples in the order of descending total file size

--- ngs_pipeline/cmds/run_sample_variant_caller.py
+++ ngs_pipeline/cmds/run_sample_variant_caller.py
@@ -128,6 +128,10 @@ def run_sample_variant_caller(args):
 
     # import IPython; IPython.embed()
 
+    # sort by descending file size
+    file_sizes = [ int(open(f'{sample}/reads/filesize').read()) for sample in samples]
+    samples = list(zip(*(sorted(zip(file_sizes, samples), reverse=True))))[1]
+
     start_time = datetime.datetime.now()
     cmds = ['parallel',
             '--eta',
