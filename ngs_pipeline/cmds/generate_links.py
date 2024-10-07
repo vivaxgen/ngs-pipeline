05f6149724d78b1fd3601e491cc896ea78774110
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-08 00:49:54 +0930

    update: reformat with black style

--- ngs_pipeline/cmds/generate_links.py
+++ ngs_pipeline/cmds/generate_links.py
@@ -1,4 +1,4 @@
-__copyright__ = '''
+__copyright__ = """
 generate_links.py - ngs-pipeline command line
 [https://github.com/vivaxgen/ngs-pipeline]
 
@@ -7,7 +7,7 @@ generate_links.py - ngs-pipeline command line
 All right reserved.
 This software is licensed under MIT license.
 Please read the README.txt of this software.
-'''
+"""
 
 # to improve the responsiveness during bash autocomplete, do not import heavy
 # modules (such as numpy, pandas, etc) here, but instead import them within the
@@ -19,23 +19,47 @@ from ngs_pipeline import cerr, cexit, arg_parser, check_NGSENV_BASEDIR
 
 
 def init_argparser():
-    p = arg_parser(desc='generate symbolic links for each of input files')
-    p.add_argument('-o', '--outdir', default='analysis',
-                   help='the name of the output directory [infiles]')
+    p = arg_parser(desc="generate symbolic links for each of input files")
+    p.add_argument(
+        "-o",
+        "--outdir",
+        default="analysis",
+        help="the name of the output directory [infiles]",
+    )
 
     m = p.add_mutually_exclusive_group()
-    m.add_argument('-s', '--single', default=False, action='store_true',
-                   help='fastq files are single (non-paired) such as ONT reads')
-    m.add_argument('-p', '--paired', default=False, action='store_true',
-                   help='fastq files are paired such as Illumina paired-end reads')
+    m.add_argument(
+        "-s",
+        "--single",
+        default=False,
+        action="store_true",
+        help="fastq files are single (non-paired) such as ONT reads",
+    )
+    m.add_argument(
+        "-p",
+        "--paired",
+        default=False,
+        action="store_true",
+        help="fastq files are paired such as Illumina paired-end reads",
+    )
 
-    p.add_argument('-u', '--underscore', type=int, default=0,
-                   help='the number of undercore for filename splitting, counted in reverse')
+    p.add_argument(
+        "-u",
+        "--underscore",
+        type=int,
+        default=0,
+        help="the number of undercore for filename splitting, counted in reverse",
+    )
 
-    p.add_argument('--use-absolute-link', default=False, action='store_true',
-                   help='Use absolute link instead of relative link')
-    p.add_argument('infiles', nargs='+',
-                   help='input files in compressed FASTQ format (.fastq.gz)')
+    p.add_argument(
+        "--use-absolute-link",
+        default=False,
+        action="store_true",
+        help="Use absolute link instead of relative link",
+    )
+    p.add_argument(
+        "infiles", nargs="+", help="input files in compressed FASTQ format (.fastq.gz)"
+    )
     return p
 
 
@@ -45,7 +69,7 @@ def generate_links(args):
     import pathlib
     from ngs_pipeline import fileutils
 
-    cerr(f'[Receiving {len(args.infiles)} source files]')
+    cerr(f"[Receiving {len(args.infiles)} source files]")
 
     if args.single:
         mode = fileutils.ReadMode.SINGLETON
@@ -58,8 +82,10 @@ def generate_links(args):
     read_files = fileutils.ReadFileDict(args.infiles, args.underscore, mode)
 
     if any(read_files.err_files):
-        cexit('ERROR: invalid input files: \n' +
-            '\n'.join(f'  {errmsg}' for errmsg in read_files.err_files))
+        cexit(
+            "ERROR: invalid input files: \n"
+            + "\n".join(f"  {errmsg}" for errmsg in read_files.err_files)
+        )
 
     # for each sample, process file
     counter = 0
@@ -69,19 +95,22 @@ def generate_links(args):
         if len(items) > 1:
             # accommodate by addding ~idx (tilde), eg: sample-01~0_R1.fastq.gz
             for idx, item in enumerate(items):
-                fileutils.make_sample_symlink(sample, item, args.outdir, idx,
-                                              use_absolute=args.use_absolute_link)
+                fileutils.make_sample_symlink(
+                    sample, item, args.outdir, idx, use_absolute=args.use_absolute_link
+                )
         else:
-            fileutils.make_sample_symlink(sample, items[0], args.outdir,
-                                          use_absolute=args.use)
-        
-        cerr(f'Generated symlink for sample {sample}')
+            fileutils.make_sample_symlink(
+                sample, items[0], args.outdir, use_absolute=args.use
+            )
+
+        cerr(f"Generated symlink for sample {sample}")
         counter += 1
 
-    cerr(f'Generated symlink for {counter} sample(s).')
+    cerr(f"Generated symlink for {counter} sample(s).")
 
 
 def main(args):
     generate_links(args)
+
 
 # EOF
