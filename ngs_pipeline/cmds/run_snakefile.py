696b68ff639d710c46afb658cbf5235913f451eb
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-08 07:09:46 +0930

    update: reformat with black

--- ngs_pipeline/cmds/run_snakefile.py
+++ ngs_pipeline/cmds/run_snakefile.py
@@ -1,29 +1,32 @@
-
 import os
-from ngs_pipeline import (cerr, cexit, arg_parser,
-                          check_NGSENV_BASEDIR, check_NGS_PIPELINE_BASE,
-                          get_snakefile_path, setup_config, snakeutils)
+from ngs_pipeline import (
+    cerr,
+    cexit,
+    arg_parser,
+    check_NGSENV_BASEDIR,
+    check_NGS_PIPELINE_BASE,
+    get_snakefile_path,
+    setup_config,
+    snakeutils,
+)
 
 
 def init_argparser(desc=None):
-    p = snakeutils.init_argparser(desc=desc or 'run arbitrary snakefile')
+    p = snakeutils.init_argparser(desc=desc or "run arbitrary snakefile")
     return p
 
 
 def run_snakefile(args, config: dict = {}):
 
-    status, elapsed_time = snakeutils.run_snakefile(
-        args,
-        config=setup_config(config)
-    )
+    status, elapsed_time = snakeutils.run_snakefile(args, config=setup_config(config))
 
     if not status:
-        cerr(f'[WARNING: snakefile {args.snakefile} did not successfully '
-             'complete]')
-    cerr(f'[Finish running snakefile {args.snakefile} (time: {elapsed_time})]')
+        cerr(f"[WARNING: snakefile {args.snakefile} did not successfully " "complete]")
+    cerr(f"[Finish running snakefile {args.snakefile} (time: {elapsed_time})]")
 
 
 def main(args):
     run_snakefile(args)
+
 
 # EOF
