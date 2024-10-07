855ed4d5e6695949ea71fb6c97cf183fd278ea0e
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-08 07:16:52 +0930

    update: reformat with black

--- ngs_pipeline/cmds/run_indv_varcall.py
+++ ngs_pipeline/cmds/run_indv_varcall.py
@@ -1,4 +1,4 @@
-__copyright__ = '''
+__copyright__ = """
 run_indv_varcall.py - ngs-pipeline command line
 [https://github.com/vivaxgen/ngs-pipeline]
 
@@ -7,7 +7,7 @@ run_indv_varcall.py - ngs-pipeline command line
 All right reserved.
 This software is licensed under MIT license.
 Please read the README.txt of this software.
-'''
+"""
 
 # to improve the responsiveness during bash autocomplete, do not import heavy
 # modules (such as numpy, pandas, etc) here, but instead import them within the
@@ -20,12 +20,13 @@ from ngs_pipeline import cerr, cexit, snakeutils
 
 # usage: run_varcall.py
 
+
 def init_argparser():
-    p = snakeutils.init_argparser(desc='run individual sample variant caller')
-    p.arg_dict['snakefile'].choices = ['var_call.smk']
-    p.arg_dict['snakefile'].default = 'var_call.smk'
-    p.add_argument('target')
-    p.add_argument('sample')
+    p = snakeutils.init_argparser(desc="run individual sample variant caller")
+    p.arg_dict["snakefile"].choices = ["var_call.smk"]
+    p.arg_dict["snakefile"].default = "var_call.smk"
+    p.add_argument("target")
+    p.add_argument("sample")
 
     return p
 
@@ -37,18 +38,21 @@ def run_indv_varcall(args):
     # simple sanity checks
     cwd = pathlib.Path.cwd()
     sample = cwd.parts[-1]
-    if not (cwd / 'reads').is_dir():
+    if not (cwd / "reads").is_dir():
         cexit(f'ERROR: current directory {cwd} does not have "reads" directory!')
 
     # remove .completed or .uncompleted screen first if exists
-    (cwd / '.completed').unlink(missing_ok=True)
-    (cwd / '.uncompleted').unlink(missing_ok=True)
+    (cwd / ".completed").unlink(missing_ok=True)
+    (cwd / ".uncompleted").unlink(missing_ok=True)
 
     status, elapsed_time = snakeutils.run_snakefile(args)
-    open(cwd / ('.uncompleted' if not status else '.completed'), 'w').write(f'{elapsed_time}')
+    open(cwd / (".uncompleted" if not status else ".completed"), "w").write(
+        f"{elapsed_time}"
+    )
 
 
 def main(args):
     run_indv_varcall(args)
+
 
 # EOF
