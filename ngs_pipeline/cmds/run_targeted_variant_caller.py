628541b9e05b36e0276b03896442275a84249a74
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-08 10:27:14 +0930

    update: reformat with black

--- ngs_pipeline/cmds/run_targeted_variant_caller.py
+++ ngs_pipeline/cmds/run_targeted_variant_caller.py
@@ -1,4 +1,4 @@
-__copyright__ = '''
+__copyright__ = """
 run_targeted_variant_caller.py - ngs-pipeline command line
 [https://github.com/vivaxgen/ngs-pipeline]
 
@@ -7,34 +7,40 @@ run_targeted_variant_caller.py - ngs-pipeline command line
 All right reserved.
 This software is licensed under MIT license.
 Please read the README.txt of this software.
-'''
+"""
 
 # to improve the responsiveness during bash autocomplete, do not import heavy
 # modules (such as numpy, pandas, etc) here, but instead import them within the
 # functions that requires respective heavy modules
 
 import os
-from ngs_pipeline import cerr, cexit, check_multiplexer, snakeutils
+from ngs_pipeline import cerr, cexit, check_multiplexer
 
 
 def init_argparser():
-    p = snakeutils.init_argparser(desc='run targeted variant calling')
-    p.arg_dict['snakefile'].choices = [
-        'panel_varcall_pe.smk',
-        'panel_varcall_lr.smk',
-        'msf_panel_varcall_pe.smk',
-        'msf_panel_varcall_lr.smk'
+    p = snakeutils.init_argparser(desc="run targeted variant calling")
+    p.arg_dict["snakefile"].choices = [
+        "panel_varcall_pe.smk",
+        "panel_varcall_lr.smk",
+        "msf_panel_varcall_pe.smk",
+        "msf_panel_varcall_lr.smk",
     ]
 
     # input/output options
-    p.add_argument('-u', '--underscore', default=0, type=int,
-                   help='number of undercore character to be stripped, '
-                   'counted in reverse')
+    p.add_argument(
+        "-u",
+        "--underscore",
+        default=0,
+        type=int,
+        help="number of undercore character to be stripped, " "counted in reverse",
+    )
 
-    p.add_argument('-o', '--outdir', default='analysis',
-                   help='directory for output [analysis/]')
-    p.add_argument('infiles', nargs='+',
-                   help='FASTQ input files, eg. sample-1.fastq.gz')
+    p.add_argument(
+        "-o", "--outdir", default="analysis", help="directory for output [analysis/]"
+    )
+    p.add_argument(
+        "infiles", nargs="+", help="FASTQ input files, eg. sample-1.fastq.gz"
+    )
 
     return p
 
@@ -46,23 +52,23 @@ def run_targeted_variant_caller(args, optional_config={}):
 
     # get default snakefile from environment if none is provided
     if not args.snakefile:
-        args.snakefile = os.environ.get('DEFAULT_SNAKEFILE', None)
+        args.snakefile = os.environ.get("DEFAULT_SNAKEFILE", None)
         if args.snakefile:
-            cerr(f'Obtaining snakefile from SNAKEFILE environment: {args.snakefile}')
+            cerr(f"Obtaining snakefile from SNAKEFILE environment: {args.snakefile}")
 
-    config = dict(
-        infiles=args.infiles,
-        underscore=args.underscore,
-        outdir=args.outdir
-    ) | optional_config
+    config = (
+        dict(infiles=args.infiles, underscore=args.underscore, outdir=args.outdir)
+        | optional_config
+    )
     status, elapsed_time = snakeutils.run_snakefile(args, config=config)
 
     if not status:
-        cerr('[WARNING: targeted variant calling did not successfully complete]')
-    cerr(f'[Finish targeted variant calling (time: {elapsed_time})]')
+        cerr("[WARNING: targeted variant calling did not successfully complete]")
+    cerr(f"[Finish targeted variant calling (time: {elapsed_time})]")
 
 
 def main(args):
     run_targeted_variant_caller(args)
+
 
 # EOF
