cf9b621ce2fded5b843de7a5cff9b4e71badb7be
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-10-09 07:03:04 +0930

    update: reformat with black

--- ngs_pipeline/cmds/generate_manifest.py
+++ ngs_pipeline/cmds/generate_manifest.py
@@ -1,4 +1,4 @@
-__copyright__ = '''
+__copyright__ = """
 generate_manifest.py - ngs-pipeline command line
 [https://github.com/vivaxgen/ngs-pipeline]
 
@@ -7,7 +7,7 @@ generate_manifest.py - ngs-pipeline command line
 All right reserved.
 This software is licensed under MIT license.
 Please read the README.txt of this software.
-'''
+"""
 
 # to improve the responsiveness during bash autocomplete, do not import heavy
 # modules (such as numpy, pandas, etc) here, but instead import them within the
@@ -20,38 +20,66 @@ from ngs_pipeline import cexit, cerr, arg_parser
 
 
 def init_argparser():
-    p = arg_parser('generate sample manifest file')
-    p.add_argument('-o', '--outfile', default='outfile.tsv')
+    p = arg_parser("generate sample manifest file")
+    p.add_argument("-o", "--outfile", default="outfile.tsv")
 
     m = p.add_mutually_exclusive_group()
-    m.add_argument('-s', '--single', default=False, action='store_true',
-                   help='fastq files are single (non-paired) such as ONT reads')
-    m.add_argument('-p', '--paired', default=False, action='store_true',
-                   help='fastq files are paired such as Illumina paired-end reads')
+    m.add_argument(
+        "-s",
+        "--single",
+        default=False,
+        action="store_true",
+        help="fastq files are single (non-paired) such as ONT reads",
+    )
+    m.add_argument(
+        "-p",
+        "--paired",
+        default=False,
+        action="store_true",
+        help="fastq files are paired such as Illumina paired-end reads",
+    )
 
-    p.add_argument('-u', '--underscore', type=int, default=0,
-                   help='number of consecutive underscore to be stripped from'
-                   'filenames to form sample code, counted in reverse')
+    p.add_argument(
+        "-u",
+        "--underscore",
+        type=int,
+        default=0,
+        help="number of consecutive underscore to be stripped from"
+        "filenames to form sample code, counted in reverse",
+    )
 
-    p.add_argument('--pause', type=int, default=0,
-                   help='pause (in seconds) before back to shell prompt, '
-                   'useful for automatic or batch processing so users can '
-                   'double check the sample names')
-    p.add_argument('--ask-confirmation', default=False, action='store_true',
-                   help='ask confirmation to continue saving to output file')
+    p.add_argument(
+        "--pause",
+        type=int,
+        default=0,
+        help="pause (in seconds) before back to shell prompt, "
+        "useful for automatic or batch processing so users can "
+        "double check the sample names",
+    )
+    p.add_argument(
+        "--ask-confirmation",
+        default=False,
+        action="store_true",
+        help="ask confirmation to continue saving to output file",
+    )
 
-    p.add_argument('-i', '--initial-manifest', default=None, type=str,
-                   help='optional initial manifest file to be appended')
+    p.add_argument(
+        "-i",
+        "--initial-manifest",
+        default=None,
+        type=str,
+        help="optional initial manifest file to be appended",
+    )
 
-    p.add_argument('infiles', nargs='*')
+    p.add_argument("infiles", nargs="*")
     return p
 
 
 def get_sample_name(filename, underline=0):
     filename = pathlib.Path(filename)
     if underline != 0:
-        return filename.name.rsplit('_', underline)[0]
-    return filename.name.removesuffix('.fastq.gz')
+        return filename.name.rsplit("_", underline)[0]
+    return filename.name.removesuffix(".fastq.gz")
 
 
 def generate_manifest(args):
@@ -62,14 +90,14 @@ def generate_manifest(args):
 
     initial_df = None
     if args.initial_manifest:
-        cerr(f'[Reading initial manifest file: {args.initial_manifest}]')
+        cerr(f"[Reading initial manifest file: {args.initial_manifest}]")
         initial_df = pd.read_table(args.initial_manifest)
-        if not (('SAMPLE' in initial_df.columns) and ('FASTQ' in initial_df.columns)):
-            cexit('[Initial manifest file does not have SAMPLE and/or FASTQ columns]')
+        if not (("SAMPLE" in initial_df.columns) and ("FASTQ" in initial_df.columns)):
+            cexit("[Initial manifest file does not have SAMPLE and/or FASTQ columns]")
 
-    cerr(f'[Receiving {len(args.infiles)} source files]')
+    cerr(f"[Receiving {len(args.infiles)} source files]")
     if (initial_df is None) and (len(args.infiles) == 0):
-        cexit('[No source files as input, please check your input files]')
+        cexit("[No source files as input, please check your input files]")
 
     if args.single:
         mode = fileutils.ReadMode.SINGLETON
@@ -81,8 +109,10 @@ def generate_manifest(args):
     read_files = fileutils.ReadFileDict(args.infiles, args.underscore, mode)
 
     if any(read_files.err_files):
-        cexit('ERROR: invalid input files: \n' +
-            '\n'.join(f'  {errmsg}' for errmsg in read_files.err_files))
+        cexit(
+            "ERROR: invalid input files: \n"
+            + "\n".join(f"  {errmsg}" for errmsg in read_files.err_files)
+        )
 
     # for each sample, process manifest line
     sample_series = []
@@ -91,35 +121,38 @@ def generate_manifest(args):
     for sample in read_files.samples():
         sample_series.append(sample)
         items = [
-            ','.join(item) if type(item) == tuple else item for item in read_files[sample]
+            ",".join(item) if type(item) == tuple else item
+            for item in read_files[sample]
         ]
-        fastq_series.append(';'.join(items))
+        fastq_series.append(";".join(items))
 
     df = pd.DataFrame(dict(SAMPLE=sample_series, FASTQ=fastq_series))
     if initial_df is not None:
         df = pd.concat([initial_df, df])
 
     # print at least 5 rows
-    cerr('[Showing snippets of sample(s):]')
+    cerr("[Showing snippets of sample(s):]")
     cerr(str(df))
-    cerr('If the sample code is not correct, try to use option --underscore')
+    cerr("If the sample code is not correct, try to use option --underscore")
 
     if args.pause > 0:
         import time
-        cerr(f'[Pausing for {args.pause} second(s) for manual inspection]')
-        cerr('[Press CTRL-C to abort saving the output file]')
+
+        cerr(f"[Pausing for {args.pause} second(s) for manual inspection]")
+        cerr("[Press CTRL-C to abort saving the output file]")
         time.sleep(args.pause)
-    
+
     if args.ask_confirmation:
-        resp = input(f'Continue saving to {args.outfile} [y/n]: ')
-        if resp.lower().strip()[0] != 'y':
+        resp = input(f"Continue saving to {args.outfile} [y/n]: ")
+        if resp.lower().strip()[0] != "y":
             return
 
-    df.to_csv(args.outfile, sep='\t', index=False)
-    cerr(f'[Writing {len(df)} sample manifest to {args.outfile}]')
+    df.to_csv(args.outfile, sep="\t", index=False)
+    cerr(f"[Writing {len(df)} sample manifest to {args.outfile}]")
 
 
 def main(args):
     generate_manifest(args)
+
 
 # EOF
