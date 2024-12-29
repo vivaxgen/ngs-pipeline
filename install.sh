9b3006eb497737bd162ce25c8ba0f60224d25d90
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2024-12-29 16:55:22 +0930

    update: split dep installation to inst-deps.sh

--- install.sh
+++ install.sh
@@ -37,74 +37,14 @@ PYVER="${PYVER:-3.12}"
 SNAKEMAKEVER="${SNAKEMAKEVER:-9}"
 source <(curl -L https://raw.githubusercontent.com/vivaxgen/vvg-box/main/install.sh)
 
-OMIT="${OMIT:-}"
-
-if ! [[ "$OMIT" =~ GATK ]]; then
-  echo "Installing the latest GATK"
-  retry 5 micromamba -y install GATK4 -c conda-forge -c bioconda
-fi
-
-echo "Installing latest htslib tools"
-micromamba -y install "bcftools>=1.18" "samtools>=1.18" -c conda-forge -c bioconda -c defaults
-
-echo "Installing latest fastp"
-micromamba -y install fastp -c conda-forge -c bioconda
-
-echo "Installing latest bwa-mem2"
-micromamba -y install bwa-mem2 -c conda-forge -c bioconda
-
-echo "Installing latest bwa"
-micromamba -y install bwa -c conda-forge -c bioconda
-
-echo "Installing minimap2"
-micromamba -y install minimap2 -c conda-forge -c bioconda -c defaults
-
-echo "Installing bowtie2"
-micromamba -y install bowtie2 -c conda-forge -c bioconda -c defaults
-
-echo "Installing freebayes"
-micromamba -y install freebayes=1.3.6 -c conda-forge -c bioconda -c defaults
-
-echo "Installing vcftools"
-micromamba -y install vcftools -c conda-forge -c bioconda
-
-echo "Installing chopper"
-micromamba -y install chopper -c conda-forge -c bioconda -c defaults
-
-echo "Installing snpEff"
-micromamba -y install snpeff -c conda-forge -c bioconda -c defaults
-
-echo "Installing sambamba"
-micromamba -y install sambamba -c conda-forge -c bioconda -c defaults
-
-echo "Installing fastqc"
-micromamba -y install fastqc -c conda-forge -c bioconda -c defaults
-
-echo "Installing required Python modules"
-pip3 install "snakemake<${SNAKEMAKEVER}"
-pip3 install snakemake-executor-plugin-cluster-generic
-pip3 install cyvcf2
-pip3 install pysam
-pip3 install pandas
-pip3 install Pillow
-pip3 install IPython
-pip3 install matplotlib
-pip3 install multiqc
-pip3 install cutadapt
-
-# pip3 install pycairo
-# we use conda pycairo since pip pycairo does not have complete binary
-# distribution
-micromamba -y install pycairo -c conda-forge
-
-pip3 install NanoPlot
-pip3 install argcomplete
-pip3 install openpyxl
-
 echo "Cloning vivaxGEN ngs-pipeline"
 git clone --depth 1 https://github.com/vivaxgen/ngs-pipeline.git ${ENVS_DIR}/ngs-pipeline
 ln -sr ${ENVS_DIR}/ngs-pipeline/etc/bashrc.d/10-ngs-pipeline ${BASHRC_DIR}/
 ln -sr ${ENVS_DIR}/ngs-pipeline/etc/bashrc.d/95-prompt-history ${BASHRC_DIR}/
+
+source ${ENVS_DIR}/ngs_pipeline/etc/inst-scripts/inst-deps.sh
+
+echo "ngs-pipeline" >> ${ETC_DIR}/installed-repo.txt
 
 echo
 echo "vivaxGEN ngs-pipeline has been successfully installed. "
