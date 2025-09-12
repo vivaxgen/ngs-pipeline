5ba43e9e7e08bcd2f1568ba9cd8de6c2ee76d49d
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2025-09-13 09:21:52 +0930

    update: latest subcommands.py from py-subcommand

--- ngs_pipeline/subcommands.py
+++ ngs_pipeline/subcommands.py
@@ -3,7 +3,7 @@
 
 __copyright__ = "(c) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com>"
 __license__ = "MIT"
-__version__ = '2024.05.23.02'
+__version__ = "2025.09.13.01"
 
 # this module provides subcommands, eg. PROG subcommand [options]
 
@@ -34,7 +34,7 @@ def _cexit(msg: str, exit_code: int = 1):
     sys.exit(exit_code)
 
 
-def arg_parser(desc: str = ''):
+def arg_parser(desc: str = ""):
     p = argparse.ArgumentParser(description=desc)
     return add_debug_to_parser(p)
 
@@ -42,8 +42,12 @@ def arg_parser(desc: str = ''):
 def add_debug_to_parser(p: argparse.ArgumentParser):
 
     try:
-        p.add_argument('--debug', action='store_true', default=False,
-                       help='open ipdb when uncatched exception is raised')
+        p.add_argument(
+            "--debug",
+            action="store_true",
+            default=False,
+            help="open ipdb when uncatched exception is raised",
+        )
     except argparse.ArgumentError:
         pass
 
@@ -54,44 +58,40 @@ class SubCommands(object):
 
     def __init__(
         self,
-
         # list of Python module names to check for commands, eg:
         # ['ngs_pipeline.cmds'] or ['seqpy.mds']
         # the order implies precedence
         modules: list[str] = [],
-
         # environment variable name to be used to get list of Python modules
         # to check for commands, the order implies precedence
         module_env: str | None = None,
-
         # whether mdules named in env variable takes precedence
         env_takes_precedence: bool = False,
-
         # set true to allow running scripts located in any directory
         allow_any_script: bool = False,
-
         # set true to allow for IPython shell
         allow_shell: bool = False,
-
         # set for specific greet, usage and help functions
         greet_func: Callable | None = None,
         usage_func: Callable | None = None,
-        help_func: Callable | None = None
+        help_func: Callable | None = None,
+        # additional copyright
+        copyright: str | None = None,
     ):
 
-        L.debug('initializing SubCommands class')
+        L.debug("initializing SubCommands class")
         self.allow_any_script = allow_any_script
         self.allow_shell = allow_shell
         self.greet = greet_func or self.generic_greet
         self.usage = usage_func or self.generic_usage
         self.help = help_func or self.generic_help
-        self.prog_name = sys.argv[0].rsplit('/', 1)[-1]
+        self.prog_name = sys.argv[0].rsplit("/", 1)[-1]
+        self.copyright = copyright
 
         # set up module list
         self.modules = modules
         if module_env:
-            module_envs = [m for m in os.environ.get(module_env, '').split(':')
-                           if m]
+            module_envs = [m for m in os.environ.get(module_env, "").split(":") if m]
             if env_takes_precedence:
                 self.modules = module_envs + self.modules
             else:
@@ -108,30 +108,36 @@ class SubCommands(object):
             completions = sorted(cmds)
 
         else:
-            completions = sorted(
-                [opt for opt in cmds if opt.startswith(last_token)]
-            )
+            completions = sorted([opt for opt in cmds if opt.startswith(last_token)])
 
         # send results through fd 8
-        ifs = os.environ.get('IFS', '\013')
-        out_stream = os.fdopen(8, 'w')
+        ifs = os.environ.get("IFS", "\013")
+        out_stream = os.fdopen(8, "w")
         out_stream.write(ifs.join(completions))
         sys.exit(0)
 
     def generic_usage(self):
+        if self.copyright:
+            _cerr(self.copyright)
         _cexit(
-            f'  usage:\n'
-            f'      {self.prog_name} CMD [OPTIONS]\n'
-            f'      {self.prog_name} [-i] [-l] [-h] [-v]\n'
-            f'  try: {self.prog_name} -l'
+            f"  usage: {self.prog_name} [-i] [-l] [-h]\n"
+            f"      {self.prog_name} CMD [ARGS]      run command or script (with ext .py)\n"
+            f"      {self.prog_name} -i              run in interactive mode\n"
+            f"      {self.prog_name} -l              list available commands\n"
+            f"      {self.prog_name} -h              show this help message\n"
+            f"  try: {self.prog_name} -l"
         )
 
     def generic_greet(self):
-        _cerr(f'{self.prog_name} - command line interface')
-        _cerr(f'Host: {platform.uname().node}')
+        _cerr(f"{self.prog_name} - command line interface")
+        _cerr(f"Host: {platform.uname().node}")
 
     def generic_help(self):
         self.generic_usage()
+
+    def version(self):
+        _cerr(f"subcommand version: {__version__}")
+        _cerr(f"try version command: {self.prog_name} version")
 
     def get_command_list(self) -> list[str]:
         # read sqpy.cmds directory
@@ -142,35 +148,35 @@ class SubCommands(object):
             cmd_files += cmd_directory.iterdir()
         cmds = []
         for p in cmd_files:
-            if p.suffix != '.py':
+            if p.suffix != ".py":
                 continue
-            if p.name.startswith('_'):
+            if p.name.startswith("_"):
                 continue
-            if '-' in p.name:
-                _cerr('Warning: found command module filename containing dash: '
-                      f'{p.name}')
+            if "-" in p.name:
+                _cerr(
+                    "Warning: found command module filename containing dash: "
+                    f"{p.name}"
+                )
                 continue
-            cmds.append(p.stem.replace('_', '-'))
+            cmds.append(p.stem.replace("_", "-"))
         return sorted(set(cmds))
 
     def show_commands(self):
-        _cout('Available commands:')
+        _cout("Available commands:")
         for cmd in self.get_command_list():
-            _cout(f'  {cmd}')
+            _cout(f"  {cmd}")
         sys.exit(0)
 
     def run_main(
-        self,
-        main: Callable | None,
-        init_argparser: Callable | None,
-        args: list[str]
+        self, main: Callable | None, init_argparser: Callable | None, args: list[str]
     ):
 
         if init_argparser is not None:
             parser = init_argparser()
             if not isinstance(parser, argparse.ArgumentParser):
-                _cexit('ERR: init_argparser() did not return ArgumentParser '
-                       'instance')
+                _cexit(
+                    "ERR: init_argparser() did not return ArgumentParser " "instance"
+                )
             add_debug_to_parser(parser)
 
             # perform bash autocompletion if needed
@@ -182,139 +188,87 @@ class SubCommands(object):
             if main is not None:
                 if args.debug:
                     from ipdb import launch_ipdb_on_exception
+
                     with launch_ipdb_on_exception():
-                        _cerr('WARN: running in debug mode')
+                        _cerr("WARN: running in debug mode")
                         main(args)
                 else:
                     main(args)
             else:
-                _cexit('ERR: init_argparser() exists but main() does not')
+                _cexit("ERR: init_argparser() exists but main() does not")
 
         elif main is not None:
             import inspect
-            if 'args' in inspect.signature(main).parameters:
+
+            if "args" in inspect.signature(main).parameters:
                 main(args=args)
             else:
                 main()
 
         # warn the user that nothing is being done
         else:
-            raise RuntimeError('FATAL ERROR: SubCommands.run_main() must be '
-                               'supplied with either main or init_argparser '
-                               'argument')
+            raise RuntimeError(
+                "FATAL ERROR: SubCommands.run_main() must be "
+                "supplied with either main or init_argparser "
+                "argument"
+            )
 
     def run_cmd(self, args: list[str]):
-        """ this method will run module init_argparser() and main() """
+        """this method will run module init_argparser() and main()"""
 
-        command = args[0].replace('-', '_')
+        command = args[0].replace("-", "_")
         L.debug("searching for module: %s", command)
         for module in self.modules:
             try:
-                module_name = f'{module}.{command}'
+                module_name = f"{module}.{command}"
                 M = importlib.import_module(module_name)
                 break
             except ModuleNotFoundError as err:
                 if err.name != module_name:
                     raise
         else:
-            _cexit(f'Command: {args[0]} does not exist! '
-                   f'Please check with "{self.prog_name} -l".')
+            _cexit(
+                f"Command: {args[0]} does not exist! "
+                f'Please check with "{self.prog_name} -l".'
+            )
 
-        _cerr(f'Executing: {M.__file__}')
+        _cerr(f"Executing: {M.__file__}")
         self.run_main(
-            getattr(M, 'main', None),
-            getattr(M, 'init_argparser', None),
-            args
+            getattr(M, "main", None), getattr(M, "init_argparser", None), args
         )
 
     def run_script(self, args: list[str]):
-        """ this method will run an arbitrary python script """
+        """this method will run an arbitrary python script"""
 
         path = args[0]
-        _cerr(f'Executing: {path}')
+        _cerr(f"Executing: {path}")
 
         # expand home directory
-        if path.startswith('~'):
+        if path.startswith("~"):
             path = pathlib.Path(path).expanduser()
 
+        if not path.exists():
+            _cexit(f"Script: {path} does not exist!")
+
         with open(path) as fh:
-            code = compile(fh.read(), path, 'exec')
+            code = compile(fh.read(), path, "exec")
             exec(code, globals())
             _g = globals()
-            _g['__name__'] = '__anyscript_main__'
-            self.run_main(
-                _g.get('main', None),
-                _g.get('init_argparser', None),
-                args
-            )
-
-    def list_repo_hashes(self, to_dict = False):
-        """list the git hashes of the environments"""
-        if to_dict:
-            _cout = lambda msg: None
-            _cerr = lambda msg: None
-        else:
-            _cout = globals()['_cout']
-            _cerr = globals()['_cerr']
-        envs_dict = {}
-        _cout('Installed environments and their git hashes:')
-        import git
-        envs_root = os.path.join(os.environ.get('VVG_BASEDIR', ''), 'envs')
-        envs = os.listdir(envs_root)
-        for env in envs:
-            env_path = os.path.join(envs_root, env)
-            if not os.path.isdir(env_path):
-                continue
-            try:
-                repo = git.Repo(env_path)
-                rwriter = repo.config_writer("global")
-                existing_safe_dirs = None
-                to_restore = ""
-                if rwriter.has_section('safe'):
-                    to_restore = "safe"
-                    if rwriter.has_option('safe', 'directory'):
-                        to_restore += ".directory"
-                        existing_safe_dirs = rwriter.get_values('safe', 'directory', '')
-                rwriter.add_value('safe', 'directory', env_path)
-                rwriter.write()
-                repo_c = git.Repo(env_path)
-                commit = repo_c.head.commit.hexsha
-                if repo.is_dirty():
-                    envs_dict[env] = f"{commit} (edited)"
-                    _cout(f'{env}:\t{commit} (edited)')
-                else:
-                    envs_dict[env] = commit
-                    _cout(f'{env}:\t{commit}')
-                rwriter.remove_section('safe')
-                if to_restore:
-                    rwriter.add_section('safe')
-                    if 'directory' in to_restore:
-                        for item in existing_safe_dirs:
-                            rwriter.add_value('safe', 'directory', item)
-                        if not existing_safe_dirs:
-                            rwriter.set_value('safe', 'directory', '')
-                rwriter.write()
-                rwriter.release()
-            except git.exc.InvalidGitRepositoryError:
-                _cerr(f'Warning: {env} is not a valid git repository')
-        if to_dict:
-            return envs_dict
-
-
+            _g["__name__"] = "__anyscript_main__"
+            self.run_main(_g.get("main", None), _g.get("init_argparser", None), args)
 
     def main(self):
 
         tokens = []
 
-        if '_ARGCOMPLETE' in os.environ:
+        if "_ARGCOMPLETE" in os.environ:
             # we are invoked from argcomplete
-            line = os.environ.get('COMP_LINE', '')
+            line = os.environ.get("COMP_LINE", "")
             tokens = line.split()
-            if len(tokens) == 1 or (len(tokens) == 2 and
-                                    not line.endswith(' ')):
+            if len(tokens) == 1 or (len(tokens) == 2 and not line.endswith(" ")):
                 self.autocomplete(tokens)
-            os.environ['COMP_LINE'] = line.split(' ', 1)[1]
-            os.environ['COMP_POINT'] = str(len(os.environ['COMP_LINE']))
+            os.environ["COMP_LINE"] = line.split(" ", 1)[1]
+            os.environ["COMP_POINT"] = str(len(os.environ["COMP_LINE"]))
             cmd = tokens[1]
         else:
             self.greet()
@@ -322,24 +276,25 @@ class SubCommands(object):
                 self.usage()
             cmd = sys.argv[1]
 
-        if cmd == '-i' and self.allow_shell:
+        if cmd == "-i" and self.allow_shell:
             # go to interactive mode
             import IPython
+
             IPython.embed()
 
-        elif cmd == '-l':
+        elif cmd == "-l":
             # show available commands
             self.show_commands()
 
-        elif cmd == '-h':
+        elif cmd == "-h":
             # show some help
             self.help()
-        
-        elif cmd == '-v':
-            # show version
-            self.list_repo_hashes()
 
-        elif cmd.endswith('.py') and self.allow_any_script:
+        elif cmd == "-v":
+            # show version
+            self.version()
+
+        elif cmd.endswith(".py") and self.allow_any_script:
 
             # run script, with:
             #   path = cmd
