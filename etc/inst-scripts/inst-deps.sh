6a1f859da25c35ca2b8de741e5269ed0e714ce95
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2025-09-14 13:36:02 +0930

    add: clair3 sub environment

--- etc/inst-scripts/inst-deps.sh
+++ etc/inst-scripts/inst-deps.sh
@@ -9,3 +9,8 @@ fi
 
 echo "Installing other dependencies with micromamba"
 retry 5 micromamba -y install -n ${uMAMBA_ENVNAME} -f ${ENVS_DIR}/ngs-pipeline/etc/inst-scripts/env.yaml
+
+echo "Installing Clair3 as ngs-pl-clair3 sub-environment"
+retry 5 micromamba create -y -n ${uMAMBA_ENVNAME}-clair3 clair3 python=3.9.0 -c conda-forge -c bioconda -c defaults
+
+
