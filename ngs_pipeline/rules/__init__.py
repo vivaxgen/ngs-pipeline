6e136ca2ddcbab76077cfe894e978804059c1cad
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2026-07-15 10:15:32 +0930

    update: prevent including identical snakefile multiple times

--- ngs_pipeline/rules/__init__.py
+++ ngs_pipeline/rules/__init__.py
@@ -1,13 +1,39 @@
-from ngs_pipeline import cerr
+import ngs_pipeline
+from ngs_pipeline import cerr, cexit
+from ngs_pipeline.snakeutils import (
+    get_snakefile_path,
+    path_to_str,
+    set_default_rule_path,
+)
+
+# set the default rule path to ngs_pipeline
+set_default_rule_path(ngs_pipeline)
 
 
 def path(snakefile: str):
     return __path__[0] + "/" + snakefile
 
 
+__included_snakefiles__ = set()
+__void_snakefile__ = path_to_str(
+    get_snakefile_path("ngs_pipeline::void.smk", strict_mode=True)
+)
+
+
 def inc(fn):
+    if not fn:
+        cexit("include: empty filename")
+
+    fullpath = path_to_str(get_snakefile_path(fn, strict_mode=False))
+
+    if fullpath in __included_snakefiles__:
+        cerr(f"including: {fn} (already included, skipping)")
+        return __void_snakefile__
+
+    __included_snakefiles__.add(fullpath)
     cerr(f"including: {fn}")
-    return fn
+    # cerr(f"fullpath: {fullpath}")
+    return fullpath
 
 
 # EOF
