bda89d523af2d05f370f2d79df527eb18894ed5e
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2026-07-20 09:29:55 +0930

    update: use lazy list helper and autocompleter

--- ngs_pipeline/cmds/run_targeted_variant_caller.py
+++ ngs_pipeline/cmds/run_targeted_variant_caller.py
@@ -19,16 +19,26 @@ from ngs_pipeline.cmds import run_snakefile
 
 
 def init_argparser():
+
+    def list_snakefiles():
+        cerr("I was called.. hiks")
+        snakefiles = [
+            "ngs_pipeline::msf/panel_varcall_pe.smk",
+            "ngs_pipeline::msf/panel_varcall_lr.smk",
+        ]
+        return snakefiles
+
     p = run_snakefile.init_argparser(desc="run targeted variant calling")
-    #p.arg_dict["snakefile"].choices = [
+    # p.arg_dict["snakefile"].choices = [
     #    "msf_targeted_varcall.smk",
     #    "panel_varcall_pe.smk",
     #    "panel_varcall_lr.smk",
     #    "msf_panel_varcall_pe.smk",
     #    "msf_panel_varcall_lr.smk",
     #    "msf_panel_varcall_null_pe.smk",
-    #]
-    p.arg_dict["snakefile"].default = "msf_targeted_varcall.smk"
+    # ]
+    p.arg_dict["snakefile"].default = "ngs_pipeline::msf/panel_varcall_pe.smk"
+    p.arg_dict["snakefile"].suggestions(p.LazyProvider(list_snakefiles)).files()
 
     # input/output options
 
@@ -75,6 +85,7 @@ def init_argparser():
         "infiles", nargs="*", help="FASTQ input files, eg. sample-1.fastq.gz"
     )
 
+    p.enable_completion()
     return p
 
 
